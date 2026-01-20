39a6e65eae1f07fc8f6b545ac5f125ebd1f7d781
uw15-boop <umsunggyu@gmail.com>
2026-01-20 21:43:48 +0900

    (2026,1,20) 열거형

--- project1/source.c
+++ project1/source.c
@@ -1,8 +1,66 @@
 ﻿#include<stdio.h>
+#include<windows.h>
+#include<conio.h>
+
+#define UP 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
+
+
+void position(int x, int y)
+{
+	COORD position = { x, y };
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+}
 
 int main()
 {
-	printf("system");
+	int x = 0, y = 0;
+	 position(x, y);
+
+	 printf("★");
 	
+	char key = 0;
+
+	while (1)
+	{
+		key = _getch();
+
+		if (key == -32 || key == 0)
+		{
+			key = _getch();
+		}
+
+		position(x, y);
+		printf(" ");
+
+		switch (key)
+		{
+
+		case UP : y--;
+			break;
+
+		case LEFT : x -= 2;
+			break;
+
+		case RIGHT: x += 2;
+			break;
+
+		case DOWN : y++;
+			break;
+
+		default: printf(" ");
+			break;
+		}
+
+		system("cls");
+
+		position(x, y);
+
+		printf("★");		
+	}
+
 	return 0;
 }
